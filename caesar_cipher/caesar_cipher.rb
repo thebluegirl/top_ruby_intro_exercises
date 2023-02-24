b2fd1cd1ab3b03bb614a68fcd2d4b1ab5b384efb
Funto <funto.afuwape@gmail.com>
2023-02-24 15:59:59 +0100

    Solve caesar cipher exercise
    The #bytes, #chr, and #join methods were used to solve this.
    
    The if conditional was also used to solve this.

---
+++ caesar_cipher/caesar_cipher.rb
@@ -0,0 +1,21 @@
+def caesar_cipher(string, shift_factor)
+    char_code_arr = string.bytes.map do |char_code|
+        #To check the ascii value range and keep the shifting to only letters of the alphabet
+        if (char_code >= 65 && char_code <= 90) || (char_code >= 97 &&char_code <= 122)
+            char_code += shift_factor
+        end
+
+        #To enforce wrapping when at the end of the alphabet
+        if (char_code > 122) || (char_code < 97 && char_code > 90)
+            char_code -= 26
+        end
+
+        #To change ascii integers to characters
+        char_code.chr
+    end
+
+    p ciphered_string = char_code_arr.join
+    return ciphered_string
+end
+
+caesar_cipher("What a string!", 5)
