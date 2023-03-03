7f58d712b82a350b010dbe974b2fd861f9ed0ee7
Funto <funto.afuwape@gmail.com>
2023-03-03 15:56:51 +0100

    Create method to get buy price
    A method was created to get the lowest buying price by taking the first two elements of the array, comparing them, and repeating this until a higher price was reached at which point the method would exit and return the lower price.

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,3 +1,19 @@
 def stock_picker(stock_price_array)
-    
+  def get_buy_price(array)
+    i = 0
+    current_price = array[i]
+    buy_price_checker = current_price
+    until current_price > buy_price_checker
+      buy_price_checker = array[i]
+      i += 1
+      current_price = array[i]
+    end
+    return buy_price_checker
+  end
+
+  buy_price =  get_buy_price(stock_price_array)
+  p buy_price
+
 end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
