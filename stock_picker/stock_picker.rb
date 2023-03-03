f687cd99c8caf48d8cf6c345fb924733a1c0ddd2
Funto <funto.afuwape@gmail.com>
2023-03-03 16:14:42 +0100

    Create method to get sell price
    A method was created to find the highest possible selling price by taking all the prices from the buying price onwards, done by creating a modified array with #slice, and finding the highest value using #max

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -10,10 +10,20 @@ def stock_picker(stock_price_array)
     end
     return buy_price_checker
   end
-
   buy_price =  get_buy_price(stock_price_array)
   p buy_price
 
+  def get_sell_price(array)
+    def get_sell_price_array(array)
+      buy_price =  get_buy_price(array)
+      i = array.index(buy_price)
+      available_prices_array = array.slice(i, array.size)
+    end
+    sell_price_array = get_sell_price_array(array)
+    sell_price_array.max
+  end
+  sell_price = get_sell_price(stock_price_array)
+  p sell_price
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
