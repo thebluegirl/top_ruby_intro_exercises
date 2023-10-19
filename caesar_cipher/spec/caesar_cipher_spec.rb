ff93b1fbfa9505a8a9c3b6911f76483a38c7cd87
Funto <funto.afuwape@gmail.com>
2023-10-19 13:48:29 +0100

    Test for parameters and wrap
    Tests written to check that .caesar_cipher will work weather or not a shift factor is provided, and that the ciphering wraps form 'z' back to 'a'.

--- caesar_cipher/spec/caesar_cipher_spec.rb
+++ caesar_cipher/spec/caesar_cipher_spec.rb
@@ -1,5 +1,21 @@
 require './lib/caesar_cipher.rb'
 
 describe ".cipher" do
+  context "when two parameters are given" do
+    it "returns a cipher" do
+      expect(cipher("string", 5)).to eql "xywnsl"
+    end
+  end
 
+  context "when one parameter is given" do
+    it "returns a cipher with a shift factor of three" do
+      expect(cipher("string")).to eql "vwulqj"
+    end
+  end
+
+  context "with wrap" do
+    it "wraps from 'z' back to 'a'" do
+      expect(cipher("zebra")).to eql "cheud"
+    end
+  end
 end
